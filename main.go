79a6bf40a46f57041fbff4fdebde9df91f45f389
chez-shanpu <cheztomo513@gmail.com>
2019-12-22 18:24:20 +0900

    update LICENSE

--- main.go
+++ main.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2019 shanpu <cheztomo513@gmail.com>
+Copyright © 2019 chez-shanpu <cheztomo513@gmail.com>
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
