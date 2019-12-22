ecbb157277937acd22d6412f00cb8ed1453723c7
chez-shanpu <cheztomo513@gmail.com>
2019-12-22 12:38:57 +0900

    Add root-command

---
+++ cmd/root.go
@@ -0,0 +1,24 @@
+package cmd
+
+import (
+  "fmt"
+  "github.com/spf13/cobra"
+  "os"
+)
+
+
+var cfgFile string
+
+
+var rootCmd = &cobra.Command{
+  Use:   "repotr",
+  Short: "Convert repository to tree structure and analyze",
+  //Long: `TODO`,
+}
+
+func Execute() {
+  if err := rootCmd.Execute(); err != nil {
+    fmt.Println(err)
+    os.Exit(1)
+  }
+}
