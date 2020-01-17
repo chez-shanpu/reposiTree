1d20df87e0aaa0102cbbb136f9978ca1799e0c6a
chez-shanpu <cheztomo513@gmail.com>
2020-01-17 15:15:56 +0900

    Update file-classifier

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/chez-shanpu/reposiTree
 go 1.13
 
 require (
+	github.com/h2non/filetype v1.0.10
+	github.com/h2non/go-is-svg v0.0.0-20160927212452-35e8c4b0612c
 	github.com/spf13/cobra v0.0.5
 	github.com/spf13/viper v1.6.1
 )
--- pkg/tree/classifier.go
+++ pkg/tree/classifier.go
@@ -2,31 +2,60 @@ package tree
 
 import (
 	"errors"
+	"github.com/h2non/filetype"
+	svg "github.com/h2non/go-is-svg"
+	"io/ioutil"
 	"log"
+	"path/filepath"
 	"regexp"
+	"strings"
 )
 
-const MAX_FILETYPE = 9
+const (
+	MaxFiletype = 8
+
+	TypeOther    = 0
+	TypeSource   = 1
+	TypeMakefile = 2
+	TypeDocker   = 3
+	TypeConfig   = 4
+	TypeStatic   = 5
+	TypeDocument = 6
+	TypeImage    = 7
+)
 
 var (
-	// TODO Available in other languages
-	GoSourceRegex = regexp.MustCompile(`.*\.go$`)
-	JsSourceRegex = regexp.MustCompile(`.*\.js$`)
-	PySourceRegex = regexp.MustCompile(`.*\.py$`)
+	GoSourceRegex         = regexp.MustCompile(`.*\.go$`)
+	JsSourceRegex         = regexp.MustCompile(`.*\.js$`)
+	PythonSourceRegex     = regexp.MustCompile(`.*\.py$`)
+	JavaSourceRegex       = regexp.MustCompile(`.*\.java$`)
+	PhpSourceRegex        = regexp.MustCompile(`.*\.php$`)
+	CsharpSourceRegex     = regexp.MustCompile(`.*\.cs$`)
+	CppSourceRegex        = regexp.MustCompile(`.*\.cpp$`)
+	TypescriptSourceRegex = regexp.MustCompile(`.*\.ts$`)
+	ShellSourceRegex      = regexp.MustCompile(`.*\.sh$`)
+	CSourceRegex          = regexp.MustCompile(`.*\.c$`)
+	RubySourceRegex       = regexp.MustCompile(`.*\.rb$`)
 
-	ShellRegex      = regexp.MustCompile(`.*\.sh$`)
 	MakefileRegex   = regexp.MustCompile(`^Makefile$`)
 	DockerfileRegex = regexp.MustCompile(`^Dockerfile$`)
 	ConfigfileRegex = regexp.MustCompile(`.*\.(env|cfg)$`)
 	StaticfileRegex = regexp.MustCompile(`.*\.(html|css|scss)$`)
 	DocumentsRegex  = regexp.MustCompile(`.*\.(md|txt)$`)
-	ImageRegex      = regexp.MustCompile(`.*\.(jpeg|jpg|png|svc)$`)
 )
 
-func FileClassifier(fileName, language string) (int, error) {
-	var sourceRegex *regexp.Regexp
+func FileClassifier(filePath, language string) (int, error) {
+	buf, _ := ioutil.ReadFile(filePath)
+	if filetype.IsImage(buf) {
+		return TypeImage, nil
+	} else if filetype.IsDocument(buf) {
+		return TypeDocument, nil
+	} else if svg.Is(buf) {
+		return TypeImage, nil
+	}
 
-	switch language {
+	var sourceRegex *regexp.Regexp
+	switch strings.ToLower(language) {
 	case "go":
 		sourceRegex = GoSourceRegex
 	case "javascript":
@@ -34,30 +63,49 @@ func FileClassifier(fileName, language string) (int, error) {
 	case "js":
 		sourceRegex = JsSourceRegex
 	case "python":
-		sourceRegex = PySourceRegex
+		sourceRegex = PythonSourceRegex
+	case "java":
+		sourceRegex = JavaSourceRegex
+	case "php":
+		sourceRegex = PhpSourceRegex
+	case "c#":
+		sourceRegex = CsharpSourceRegex
+	case "cs":
+		sourceRegex = CsharpSourceRegex
+	case "cpp":
+		sourceRegex = CppSourceRegex
+	case "typescript":
+		sourceRegex = TypescriptSourceRegex
+	case "ts":
+		sourceRegex = TypescriptSourceRegex
+	case "shell":
+		sourceRegex = ShellSourceRegex
+	case "c":
+		sourceRegex = CSourceRegex
+	case "ruby":
+		sourceRegex = RubySourceRegex
+	case "rb":
+		sourceRegex = RubySourceRegex
 	default:
-		return 0, errors.New("argument --language is invalid (go, javascript, python available)")
+		return 0, errors.New("argument --language is invalid (go, javascript, python, java, php, c#, cpp,typescript, shell, c, ruby, rb available)")
 	}
 
+	_, fileName := filepath.Split(filePath)
 	switch {
 	case sourceRegex.MatchString(fileName): // Matching Sourcefiles
-		return 0, nil
-	case ShellRegex.MatchString(fileName): // Matching Shellscripts
-		return 1, nil
+		return TypeSource, nil
 	case MakefileRegex.MatchString(fileName): // Matching Makefiles
-		return 2, nil
+		return TypeMakefile, nil
 	case DockerfileRegex.MatchString(fileName): // Matching Dockerfiles
-		return 3, nil
+		return TypeDocker, nil
 	case ConfigfileRegex.MatchString(fileName): // Matching Configfiles
-		return 4, nil
+		return TypeConfig, nil
 	case StaticfileRegex.MatchString(fileName): // Matching Staticfiles
-		return 5, nil
+		return TypeStatic, nil
 	case DocumentsRegex.MatchString(fileName): // Matching Documents
-		return 6, nil
-	case ImageRegex.MatchString(fileName): // Matching Images
-		return 7, nil
+		return TypeDocument, nil
 	default: // Others
 		log.Println(fileName + " is classified as a others.")
-		return 8, nil
+		return TypeOther, nil
 	}
 }
